dbe0ff0c059722be85f25ba801a61968ec3712ee
HP <HP@192.168.15.7>
2021-04-26 19:42:52 -0300

    Segundo commit

--- ConexaoIDEComBancoDeDados/src/TestaInsercaoComParametro.java
+++ ConexaoIDEComBancoDeDados/src/TestaInsercaoComParametro.java
@@ -11,15 +11,22 @@ public class TestaInsercaoComParametro {
 	public static void main(String[] args) throws SQLException {
 		ConnectionFactory factory = new ConnectionFactory();
 		try (Connection connection = factory.recuperarConexao()) {
-			
 
+			// eu vou controlar o momento do commit da minha (transação), usamos
+			// 'AutoCommit'
 			connection.setAutoCommit(false);
 
+			
+			// Com o try com recursos (try-with-resources) não é mais necessário explicitar o close
+			// para fechar o statements (ResultSet, Connection, PreparedStatement). Pelo fato dos statements estenderem AutoCloseable
 			try (PreparedStatement stm = connection.prepareStatement(
 					"INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);) {
 				adcionarVariavel("SmartTV", "45 polegadas", stm);
 				adcionarVariavel("Radio", "Radio de bateria", stm);
-
+				
+				
+				// é necessário explicitar o commit e o rollback, para ter o controle total das
+				// transações para mostrar todos produtos adicionados ou nada.
 				connection.commit();
 
 			} catch (Exception e) {
@@ -38,7 +45,7 @@ public class TestaInsercaoComParametro {
 		stm.execute();
 
 		try (ResultSet rst = stm.getGeneratedKeys()) {
-			
+
 			while (rst.next()) {
 				Integer id = rst.getInt(1);
 				System.out.println("O id criado foi: " + id);
