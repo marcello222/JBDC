50823a899076377e9b51fffafa86ae943df5c7de
Marcello Eduardo <45945526+marcello222@users.noreply.github.com>
2021-04-27 15:19:55 -0300

    Primeiro commit

---
+++ ConexaoIDEComBancoDeDados/src/TestaListagemDeCategorias.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import conexao.Categoria;
+import conexao.ConnectionFactory;
+import conexao.Produto;
+import dao.CategoriaDAO;
+
+public class TestaListagemDeCategorias {
+
+	public static void main(String[] args) throws SQLException {
+		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
+			CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
+			List<Categoria> listaDeCategorias = categoriaDAO.listarComProdutos();
+			listaDeCategorias.stream().forEach(ct -> {
+				System.out.println(ct.getNome());
+				for (Produto produto : ct.getProdutos()) {
+					System.out.println(ct.getNome() + " - " + produto.getNome());
+				}
+			});
+		}
+
+	}
+
+}
